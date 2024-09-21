c9cab5e0fc48bc6d02b77f797746f655d80f83c4
Breno Pique <brenopique@outlook.com>
2024-09-21 00:25:09 -0300

    feat: Adicionado todos arquivos iniciais
    Adição dos arquivos iniciais do projeto

---
+++ assets/js/index/questions.js
@@ -0,0 +1,307 @@
+export default [
+  // Perguntas fáceis
+  {
+    question: "Quanto é 1 + 2?",
+    answers: [
+      { option: "3", correct: true },
+      { option: "2", correct: false },
+      { option: "4", correct: false },
+      { option: "5", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 5 x 2?",
+    answers: [
+      { option: "9", correct: false },
+      { option: "10", correct: true },
+      { option: "11", correct: false },
+      { option: "12", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 10 - 3?",
+    answers: [
+      { option: "7", correct: true },
+      { option: "6", correct: false },
+      { option: "8", correct: false },
+      { option: "5", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 4 x 2?",
+    answers: [
+      { option: "6", correct: false },
+      { option: "8", correct: true },
+      { option: "10", correct: false },
+      { option: "12", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 12 ÷ 4?",
+    answers: [
+      { option: "4", correct: false },
+      { option: "2", correct: false },
+      { option: "3", correct: true },
+      { option: "6", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 9 + 1?",
+    answers: [
+      { option: "10", correct: true },
+      { option: "9", correct: false },
+      { option: "8", correct: false },
+      { option: "11", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 6 x 1?",
+    answers: [
+      { option: "6", correct: true },
+      { option: "7", correct: false },
+      { option: "1", correct: false },
+      { option: "5", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 15 ÷ 3?",
+    answers: [
+      { option: "4", correct: false },
+      { option: "6", correct: false },
+      { option: "5", correct: true },
+      { option: "3", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 3 x 3?",
+    answers: [
+      { option: "6", correct: false },
+      { option: "9", correct: true },
+      { option: "12", correct: false },
+      { option: "8", correct: false },
+    ],
+    difficulty: 1,
+  },
+  {
+    question: "Quanto é 7 + 2?",
+    answers: [
+      { option: "8", correct: false },
+      { option: "9", correct: true },
+      { option: "10", correct: false },
+      { option: "7", correct: false },
+    ],
+    difficulty: 1,
+  },
+
+  // Perguntas médias
+  {
+    question: "Quanto é 7 x 7?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+      { option: "48", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 25 ÷ 5?",
+    answers: [
+      { option: "4", correct: false },
+      { option: "5", correct: true },
+      { option: "6", correct: false },
+      { option: "7", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 8 x 6?",
+    answers: [
+      { option: "48", correct: true },
+      { option: "42", correct: false },
+      { option: "50", correct: false },
+      { option: "44", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 12 x 11?",
+    answers: [
+      { option: "121", correct: false },
+      { option: "132", correct: true },
+      { option: "142", correct: false },
+      { option: "124", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 16 ÷ 4?",
+    answers: [
+      { option: "3", correct: false },
+      { option: "4", correct: true },
+      { option: "5", correct: false },
+      { option: "6", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 18 ÷ 6?",
+    answers: [
+      { option: "2", correct: false },
+      { option: "3", correct: true },
+      { option: "4", correct: false },
+      { option: "5", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 15 x 2?",
+    answers: [
+      { option: "25", correct: false },
+      { option: "30", correct: true },
+      { option: "20", correct: false },
+      { option: "35", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 9 x 9?",
+    answers: [
+      { option: "72", correct: false },
+      { option: "81", correct: true },
+      { option: "90", correct: false },
+      { option: "64", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 64 ÷ 8?",
+    answers: [
+      { option: "6", correct: false },
+      { option: "7", correct: false },
+      { option: "8", correct: true },
+      { option: "9", correct: false },
+    ],
+    difficulty: 2,
+  },
+  {
+    question: "Quanto é 27 ÷ 9?",
+    answers: [
+      { option: "2", correct: false },
+      { option: "3", correct: true },
+      { option: "4", correct: false },
+      { option: "5", correct: false },
+    ],
+    difficulty: 2,
+  },
+
+  // Perguntas difíceis
+  {
+    question: "Quanto é 18 x 17?",
+    answers: [
+      { option: "296", correct: false },
+      { option: "306", correct: true },
+      { option: "316", correct: false },
+      { option: "286", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 45 ÷ 3?",
+    answers: [
+      { option: "13", correct: false },
+      { option: "15", correct: true },
+      { option: "14", correct: false },
+      { option: "16", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 27 x 19?",
+    answers: [
+      { option: "513", correct: true },
+      { option: "501", correct: false },
+      { option: "521", correct: false },
+      { option: "503", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 36 x 12?",
+    answers: [
+      { option: "432", correct: true },
+      { option: "442", correct: false },
+      { option: "412", correct: false },
+      { option: "422", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 144 ÷ 12?",
+    answers: [
+      { option: "10", correct: false },
+      { option: "12", correct: true },
+      { option: "14", correct: false },
+      { option: "16", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 72 ÷ 6?",
+    answers: [
+      { option: "11", correct: false },
+      { option: "12", correct: true },
+      { option: "13", correct: false },
+      { option: "14", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 18 x 18?",
+    answers: [
+      { option: "320", correct: false },
+      { option: "324", correct: true },
+      { option: "328", correct: false },
+      { option: "318", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 98 ÷ 7?",
+    answers: [
+      { option: "13", correct: false },
+      { option: "14", correct: true },
+      { option: "15", correct: false },
+      { option: "12", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 121 ÷ 11?",
+    answers: [
+      { option: "10", correct: false },
+      { option: "11", correct: true },
+      { option: "12", correct: false },
+      { option: "13", correct: false },
+    ],
+    difficulty: 3,
+  },
+  {
+    question: "Quanto é 23 x 24?",
+    answers: [
+      { option: "552", correct: true },
+      { option: "562", correct: false },
+      { option: "542", correct: false },
+      { option: "532", correct: false },
+    ],
+    difficulty: 3,
+  },
+];
